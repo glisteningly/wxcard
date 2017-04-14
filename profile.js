e71b6240aebda46d5ff28de7595594ba0d7a2c2b
tyf <arexsm@126.com>
2017-04-14 17:13:29 +0800

    加入vue.js版的名片实现
    /profile.html?name=xxx 可查看xxx的名片

---
+++ profile.js
@@ -0,0 +1,53 @@
+products =[
+    {name:"医院综合管理信息平台", wxUrl:"KNvgbYOWmtWqIMG3hWOVLA"},
+    {name:"医院办公HOA系统", wxUrl:"W9iG1v3om0dTffTZHnf3LA"},
+    {name:"医务资源管理系统", wxUrl:"ifePsGFbmtu3bKbJvW7YDQ"},
+    {name:"院领导BI决策分析系统", wxUrl:"MbVhWJ6CeF9Av9BoB1-k0Q"},
+    {name:"医疗不良事件管理系统", wxUrl:"wrVQwwDCk34EQueP8Y2yPg"},
+    {name:"危急值管理系统", wxUrl:"gXzNshIBs8Bzzouk0E3i7Q"},
+    {name:"医院智能排班系统", wxUrl:"zeMd_8zll8z9I5bDqchwmA"},
+]
+
+profiles = {
+    "company": {
+        name: "启天集团",
+        logoImageUrl: "./images/logo.png",
+        leader: "hehui"
+    },
+    "baibh": {
+        id: "0",
+        name: "白百合",
+        position: "领衔主演",
+        phone: "13912345678",
+        qqnum: "987654",
+        wechat: "weixin12345",
+        mail: "baibaihe@sxqtyx.com",
+        avatarImageUrl: "http://www.ah.xinhuanet.com/2013-04/16/115402126_21n.jpg",
+        qrcodeImageUrl: "./images/qrcode_hehui.jpg"
+
+    },
+    "hehui": {
+        id: "1",
+        name: "何 辉",
+        position: "总经理",
+        phone: "13991386688",
+        qqnum: "460883563",
+//            wechat: "weixin12345",
+        mail: "hehui@sxqtyx.com",
+        avatarImageUrl: "./images/hehui.jpg",
+        qrcodeImageUrl: "./images/qrcode_hehui.jpg"
+
+    },
+    "qinbc": {
+        id: "2",
+        name: "秦宝成",
+        position: "市场总监",
+        phone: "13912345679",
+        qqnum: "828846572",
+        wechat: "weixin12345",
+        mail: "qinbaocheng@sxqtyx.com",
+        avatarImageUrl: "./images/qinbc.png",
+        qrcodeImageUrl: "./images/qrcode_qinbc.png"
+    },
+}
+
